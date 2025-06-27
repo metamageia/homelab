e5b6e84e5e0b7930a7ee1a57401f4512fb98ccc8
metamageia <metamageia@gmail.com>
2025-06-27 01:07:35 -0500

    Setup Bootstrap

---
+++ terraform/main/homelab-host.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_droplet" "homelab-control" {
+  image  = "191473488"
+  name   = "homelab-control"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+
+  connection {
+    host        = self.ipv4_address
+    user        = "root"
+    type        = "ssh"
+    private_key = var.pvt_key
+    timeout     = "2m"
+  }
+
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+    ]
+  }
+}
